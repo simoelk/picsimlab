0e8285773b2d4b6dbcb173dc76488ece41570189
simoelk <elkhadraouymohammed@gmail.com>
2020-07-15 10:04:30 +0200

    New: support to Curiosity HPC Board

--- src/boards/board_8.h
+++ src/boards/board_8.h
@@ -4,7 +4,7 @@
 
    ########################################################################
 
-   Copyright (c) : 2010-2015  Luis Claudio Gambôa Lopes
+   Copyright (c) : 2015-2018  Luis Claudio Gambôa Lopes
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -23,32 +23,20 @@
    For e-mail suggestions :  lcgamboa@yahoo.com
    ######################################################################## */
 
-#ifndef BOARDS_DEFS_H
-#define	BOARDS_DEFS_H
+#ifndef BOARD_8_H
+#define BOARD_8_H
+
+#include<lxrad.h>
+
+#include "board_pic.h"
+
+//New board Class derived from board class defined in board.h
+class cboard_8:public board_pic
+{
+    private:
+        int p_BT1; // Button S1
+        int p_BT2;  // Button S2
+        
 
 
-//includes of boards
-#include"board_0.h"
-#include"board_1.h" 
-#include"board_2.h"
-#include"board_3.h"
-#include"board_4.h"
-#include"board_5.h"
-//#ifdef _EXPERIMENTAL_
-#include"board_6.h"
-#include"board_7.h"
-#include"board_x.h"
-//number of last board
-#define BOARDS_LAST 9
-//#else
-//number of last board
-//#define BOARDS_LAST 6
-//#endif
-
-//boards object creation
-board * create_board(int *lab,int *lab_);
-
-extern const char boards_list[BOARDS_LAST][30];
-
-#endif	/* BOARDS_DEFS_H */
-
+}
